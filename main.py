784fd614a14138471225ce0a541c5f4f6cfaa112
Zyire <zmurphy5@broncos.uncfsu.edu>
2025-09-15 15:56:26 -0400

    This is a template for "Project Part 1"

---
+++ main.py
@@ -0,0 +1,9 @@
+game_name = ""  # TODO , name your game project
+print()  #  TODO, print greeting with game name
+print("=======================") # TODO, add or remove equal symbol to align the greetings
+
+# Ask for the character's name
+name = input()
+
+# Print the name
+print()
