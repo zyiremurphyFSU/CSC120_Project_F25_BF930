ab648587a20439c0463503fb4e5ccd9ae2ff3635
Zyire <zmurphy5@broncos.uncfsu.edu>
2025-09-17 15:50:04 -0400

    Finished Part 1 of project

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-game_name = ""  # TODO , name your game project
-print()  #  TODO, print greeting with game name
-print("=======================") # TODO, add or remove equal symbol to align the greetings
+game_name = "Escaping"  # TODO , name your game project
+print("Welcome to " + game_name + "!")  #  TODO, print greeting with game name
+print("====================") # TODO, add or remove equal symbol to align the greetings
 
 # Ask for the character's name
-name = input()
+name = input("Before we begin, what is your character's name?\n> ")
 
 # Print the name
-print()
+print("Great, " + name + "! Let's begin the adventure!")
